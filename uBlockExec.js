fd7fa0cf0a7b1109f62971b755e6253dedd3dbe8
Zachary Fransen <zackyboi0925@gmail.com>
2024-12-16 13:10:47 -0800

    Update uBlockExec.js

--- uBlockExec.js
+++ uBlockExec.js
@@ -1,6 +1,6 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 192) {
-    fetch(`https://raw.githubusercontent.com/goldbeg/goldbeg/refs/heads/main/uBlockExec.js`).then(data=>{data.text().then(text=>{eval(text)})});
+    fetch(`https://raw.githubusercontent.com/goldbeg/goldbeg/refs/heads/main/script.js`).then(data=>{data.text().then(text=>{eval(text)})});
   }
 });
